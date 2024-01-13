1d853b8007355c5bd844db0f8b80f61224bf63e8
Zahra M <105454060+zeemj26@users.noreply.github.com>
2024-01-13 14:06:19 +0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using _213002_Assignment05.ViewComponents;
+using Microsoft.AspNetCore.Builder;
+
+namespace _213002_Assignment05
+{
+    public class Startup
+    {
+        public void ConfigureServices (IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<SimpleViewComponent>();
+
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            /*app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });*/
+        }
+
+    }
+}
